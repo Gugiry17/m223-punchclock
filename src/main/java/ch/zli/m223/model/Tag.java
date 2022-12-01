c1f251ec9af272cb561cb879ee05d1f1937b0818
Micha Baumann <micha.baumann@gmx.ch>
2022-12-01 08:10:08 +0000

    added category and tag

---
+++ src/main/java/ch/zli/m223/model/Tag.java
@@ -0,0 +1,59 @@
+package ch.zli.m223.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+public class Tag {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToMany(mappedBy = "tag")
+    @JsonIgnoreProperties("tag")
+    @Fetch(FetchMode.JOIN)
+    private Set<Entry> entries;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Entry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(Set<Entry> entries) {
+        this.entries = entries;
+    }
+
+
+}
